466d676f3c1a78e8aa1bdec8a4981389010c3a7c
Ari Ramdial <ari.ramdial@gmail.com>
2016-09-14 14:51:27 -0400

    Add pebble data for all team members

--- app.py
+++ app.py
@@ -7,8 +7,8 @@ import os
 from flask import Flask
 from flask import request
 from flask import make_response
-
-rhodium_email = 'hello@rhodium.io'
+from geopy.geocoders import Nominatim
+geolocator = Nominatim()
 
 # Flask app should start in global layout
 app = Flask(__name__)
@@ -25,24 +25,74 @@ def webhook():
     return r
 
 def processHumanAPIRequest(req):
-    baseurl = "https://api.humanapi.co/v1/human/activities/summaries?access_token="
+    activityurl = "https://api.humanapi.co/v1/human/activities/summaries?access_token="
+    locationurl = "https://api.humanapi.co/v1/human/locations?access_token="
     access_token = os.environ['HUMANAPI_ACCESS_TOKEN']
-    human_url = baseurl + access_token
-    result = urllib.urlopen(human_url).read()
-    data = json.loads(result)
-    res = makeWebhookResult(parseHumanData(data))
+    activity_url = activityurl + access_token + "&source=moves&limit=1"
+    location_url = locationurl + access_token + "&source=moves&limit=1"
+    activity = urllib.urlopen(activity_url).read()
+    location = urllib.urlopen(location_url).read()
+    activity_data = json.loads(activity)
+    location_data = json.loads(location)
+    res = makeWebhookResult(parseHumanData(activity_data, location_data))
     return res
 
-def parseHumanData(data):
-    steps = data[0]["steps"]
-    calories = data[0]["calories"]
-    data = [steps, calories]
+def parseHumanData(activity_data,location_data):
+    steps = activity_data[0]["steps"]
+    calories = activity_data[0]["calories"]
+    lat = str(location_data[0]["location"]["lat"])
+    lon = str(location_data[0]["location"]["lon"])
+    coord = lat + "," + lon
+    location = city_and_state(coord)
+    city = location[0]
+    state = location[1]
+    data = [steps, calories,city, state]
     return data
+
+def city_and_state(coord):
+    location = geolocator.reverse(coord, exactly_one=True)
+    address = location.raw['address']
+    city = address.get('city', '')
+    state = address.get('state', '')
+    return city, state
 
 def makeWebhookResult(data):
     return {
+    "Ari":
+        {
         "steps": data[0],
-        "calories": data[1]
+        "calories": data[1],
+        "city": data[2],
+        "state": data[3]
+        }
+    "Alexandra":
+        {
+        "steps": data[0],
+        "calories": data[1],
+        "city": data[2],
+        "state": data[3]
+        }
+    "Daskalov":
+        {
+        "steps": data[0],
+        "calories": data[1],
+        "city": data[2],
+        "state": data[3]
+        }
+    "Nadim":
+        {
+        "steps": data[0],
+        "calories": data[1],
+        "city": data[2],
+        "state": data[3]
+        }
+    "Imran":
+        {
+        "steps": data[0],
+        "calories": data[1],
+        "city": data[2],
+        "state": data[3]
+        }
     }
 
 
